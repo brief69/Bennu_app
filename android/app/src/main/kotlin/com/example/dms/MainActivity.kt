6034e48b9a616c97587ec62937ad712fa5516472
IR <ir@Imacbook.local>
2023-11-08 23:24:39 +0900

    I sketched the whole thing out a little.

--- android/app/src/main/kotlin/com/example/dms/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dms
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/widgets/buttonwidgets/caption_button_widget.dart
+++
@@ -1,7 +0,0 @@
-
-
-// caption_button_widget.dart
-// postデータのうちの、captionを表示するボタンウィジェット。
-// captionを長押しで説明の全文が表示できる。
-
-// 
