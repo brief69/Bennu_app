582d2ffdc92af4943855aebf8e4740cd39f543c8
IR <ir@Imacbook.local>
2023-11-17 17:05:57 +0900

    Minor deletions

--- lib/services/firebase_notification_service.dart
+++ lib/services/firebase_notification_service.dart
@@ -1,6 +1,6 @@
 
 
-// notification_service.dart
+// firebase_notification_service.dart
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:firebase_messaging/firebase_messaging.dart';
